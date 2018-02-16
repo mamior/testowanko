ca99c29c44d2ba8b57866478bffdcb56d7a07324
mamior <36538074+mamior@users.noreply.github.com>
2018-02-16 20:09:10 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,39 +2,30 @@
 
 using namespace std;
 
-int n = 0, i; // liczba testow
-
+long int wynik = 1;
+unsigned int liczba;
+int dziesiatki = 0, jednosci = 0, ile_prob;
 int main(){
-	// wczytanie liczby testow
-	cout << "Prosze podac liczbe testow (n<100000): ";
-	cin >> n;
-	// sprawdzenie poprawnosci wpisanej liczby
-	if ((n > 100000) || (n <= 0)) {
-		cout << "podales za duza liczbe" << endl;
-		exit(1);
+	cout << "prosze podac ile testow [1...30]" << endl;
+	cin >> ile_prob;
+	if ((ile_prob > 30) || (ile_prob < 1)){
+		cout << "Zla ilosc testow";
+		exit(1); 
 	}
-	// wczytanie liczb do sprawdzenia
-	else{
-		cout << "Prosze podac liczby do sprawdzenia: " << endl;
-		int tablica[n] = {0};
-		for (i = 0; i < n; ++i){
-			cin >> tablica[i];			
+	
+	for (;ile_prob > 0; ile_prob--){
+		cout << "prosze podac liczbe 'n' calkowita nieujemna:" << endl;
+		cin >> liczba;
+		// obliczanie n!
+		for (int i = liczba; i > 1; i--){
+			wynik *= i;		
 		}
-	// sprawdzenie czy liczba jest pierwsza + wypisanie wyniku
-		int a, b, c;
-		for (i = 0; i < n; ++i){
-			a = tablica[i];
-			for (c = 1, b = 0; c <= a; ++c){			
-				if (a%c == 0) b++;
-				if (b > 2 || a == 1) {
-					cout << i+1 << ". NIE" << endl;
-					break;
-				}
-			}
-			if (b == 2) {
-				cout << i+1 << ". TAK" << endl;
-			}
-		}
+		cout << wynik << endl;
+		jednosci = wynik % 10;
+		dziesiatki = (wynik % 100)/10;
+		cout << "liczba dziesiatek: " << dziesiatki << endl;
+		cout << "liczba jednosci: " << jednosci << endl;
+		cout << endl;
 	}
 	return 0;
 }
